85ce92a527d9d79498916c07dbe98bd92ec6e5f8
Alex <mulvaneya7@gmail.com>
2020-05-29 13:50:29 -0700

    Added Recipe Cards using Recipe Model displayed on Category clicks

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/recipe_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryRecipes = DUMMY_MEALS.where((recipe) {
+      return recipe.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          categoryTitle,
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return RecipeItem(
+            title: categoryRecipes[index].title,
+            imageUrl: categoryRecipes[index].imageUrl,
+            duration: categoryRecipes[index].duration,
+            affordability: categoryRecipes[index].affordability,
+            complexity: categoryRecipes[index].complexity,
+          );
+        },
+        itemCount: categoryRecipes.length,
+      ),
+    );
+  }
+}
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './category_meals_screen.dart';
+import '../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
@@ -9,12 +9,12 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.title, this.color, this.id);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen(id, title);
-        },
-      ),
+    Navigator.of(ctx).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
     );
   }
 
